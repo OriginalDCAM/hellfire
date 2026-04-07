ac5e611b08dd0ea8651f3008dea22ce46c865780
TrueStatement <35526062+OriginalDCAM@users.noreply.github.com>
2026-04-07 12:19:24 +0200

    - Cleaned up the main entry point of game

---
+++ Sandbox/include/Game.h
@@ -0,0 +1,46 @@
+﻿//
+// Created by denzel on 03/04/2025.
+//
+
+#ifndef GAME_H
+#define GAME_H
+
+#include <DCraft.h>
+
+#include "Dcraft/Graphics/Primitives/Cube.h"
+
+
+class Game {
+public:
+    Game() {}
+
+    ~Game() {
+        // Scenemanager handles deletion of objects for us
+    }
+
+    void setup_callbacks(DCraft::Application &app );
+
+    bool drone_key_processed = false;
+    bool drone_mode_active = false;
+    float drone_toggle_timer = 0.0f;
+    const float DRONE_TOGGLE_COOLDOWN = 0.2f; // 200ms cooldown
+
+    void init(DCraft::Application &app);
+
+    void setup(DCraft::SceneManager &sm, DCraft::WindowInfo window);
+
+    // Input handler
+    void handle_input(DCraft::Application &app, float delta_time);
+
+    void update(float delta_time) const;
+
+private:
+    DCraft::PerspectiveCamera *camera_ = nullptr;
+    DCraft::PerspectiveCamera *drone_camera_ = nullptr;
+    DCraft::Cube *cube_ = nullptr;
+    DCraft::Scene *initial_scene;
+    DCraft::SceneManager *scene_manager_;
+};
+
+
+#endif //GAME_H
