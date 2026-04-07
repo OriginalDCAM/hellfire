bb9b299e10e8a3b03a15e2f636a3b2e5d2060b0f
TrueStatement <35526062+OriginalDCAM@users.noreply.github.com>
2026-04-07 12:23:02 +0200

    Add DisplayCase object with glass material and shader; update framebuffer binding method

---
+++ Sandbox/src/Objects/DisplayCase.cpp
@@ -0,0 +1,38 @@
+﻿//
+// Created by denzel on 05/06/2025.
+//
+#include "Objects/DisplayCase.hpp"
+
+#include "Dcraft/Graphics/Primitives/Cube.h"
+
+void DisplayCase::init() {
+    // Override the default name
+    set_name("Glass Box");
+
+    // Create the material
+    auto GlassMaterialComponent = DCraft::MaterialBuilder::create_lambert("Glass Material");
+    GlassMaterialComponent->set_texture("assets/textures/transparent/glass.jpg", DCraft::TextureType::DIFFUSE);
+    GlassMaterialComponent->set_uv_tiling(1.0f, 1.0f);
+    GlassMaterialComponent->set_ambient_color(glm::vec3(0.5, 0.5, 0.5));
+    GlassMaterialComponent->set_transparency(0.50f);
+
+    uint32_t transparent_shader = DCraft::Application::get_instance().get_shader_manager().get_shader_for_material(*GlassMaterialComponent);
+    uint32_t custom_test_shader = DCraft::Application::get_instance().get_shader_manager().load_shader_from_files("assets/shaders/standard.vert", "assets/shaders/test.frag");
+    GlassMaterialComponent->set_compiled_shader_id(custom_test_shader);
+
+    // Create the mesh
+    DCraft::Cube* MeshComponent = new DCraft::Cube("Glass Cube Mesh");
+    MeshComponent->set_rotation(glm::vec3(90.0f, 0.0f, 0.0f));
+    MeshComponent->set_scale(glm::vec3(1.0f, 1.0f, 1.0f));
+    MeshComponent->set_position(0.0f, 4.0f, 0.0f);
+    MeshComponent->set_material(GlassMaterialComponent.release());
+    
+    // Add it to this object
+    add(MeshComponent);
+}
+
+void DisplayCase::update(float delta_time) {
+    // Rotate the mesh
+    auto movement_speed = 5 * delta_time;
+    set_rotation(glm::vec3(0,movement_speed,0) + get_rotation());
+}
