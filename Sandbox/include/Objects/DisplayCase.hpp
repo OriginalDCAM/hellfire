58a154add063bb0b052ae91a54a80bb211503067
TrueStatement <35526062+OriginalDCAM@users.noreply.github.com>
2026-04-07 12:25:42 +0200

    Add DisplayCase object with glass material and shader; update framebuffer binding method

---
+++ Sandbox/include/Objects/DisplayCase.hpp
@@ -0,0 +1,13 @@
+﻿//
+// Created by denzel on 05/06/2025.
+//
+#include <DCraft.h>
+
+#pragma once
+class DisplayCase : public DCraft::Object3D {
+public:
+    void init();
+    void update(float delta_time) override;
+    
+};
+
