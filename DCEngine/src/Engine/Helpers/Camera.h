496dd4a876d3fe1e48c988e07372975244b3657c
TrueStatement <35526062+OriginalDCAM@users.noreply.github.com>
2026-04-07 12:14:35 +0200

    Add project files.

---
+++ DCEngine/src/Engine/Helpers/Camera.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "../Core/Entity.h"
+
+class Camera : public Entity
+{
+public:
+	
+};
---
+++ DCEngine/src/Engine/Lighting/Light.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Entity.h"
+
+class Light : public Entity 
+{
+public:
+	
+};
